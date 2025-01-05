735a1c864154047edf5c675df553e159eb4caa24
Shaikh1734 <shagufthashaikh29@gmail.com>
2025-01-05 16:11:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 import csv
 import datetime
@@ -20,7 +21,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 nltk_data_path = os.path.join(os.path.expanduser("~"), "nltk_data")
 nltk.data.path.append(nltk_data_path)
 try:
-    nltk.data.find('tokenizers/punkt')
+ nltk.data.find('tokenizers/punkt')
 except LookupError:
     nltk.download('punkt', download_dir=nltk_data_path)
 
